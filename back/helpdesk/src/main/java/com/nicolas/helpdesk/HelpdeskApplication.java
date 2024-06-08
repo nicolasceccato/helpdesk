8e9cedf4aa4f08df2d0bc978d33ca4d67e65f0bf
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-08 20:34:57 -0300

    initial commit

---
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,15 @@
+package com.nicolas.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
