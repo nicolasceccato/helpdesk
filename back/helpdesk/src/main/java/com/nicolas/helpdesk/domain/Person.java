e6b0bffdbe94cb2b020c1a51c34820f717e8031f
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-08 22:41:56 -0300

    feat(domain): create the class Person

---
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/domain/Person.java
@@ -0,0 +1,102 @@
+package com.nicolas.helpdesk.domain;
+
+import com.nicolas.helpdesk.domain.enums.Profile;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public abstract class Person {
+
+    protected Integer id;
+    protected String name;
+    protected String cpf;
+    protected String email;
+    protected String password;
+    protected Set<Integer> profiles = new HashSet<>();
+    protected LocalDate criationDate = LocalDate.now();
+
+    public Person() {
+        addProfile(Profile.CLIENT);
+    }
+
+    public Person(Integer id, String name, String cpf, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.password = password;
+        addProfile(Profile.CLIENT);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Profile> getProfiles() {
+        return profiles.stream().map(x -> Profile.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addProfile(Profile profile) {
+        this.profiles.add(profile.getCode());
+    }
+
+    public LocalDate getCriationDate() {
+        return criationDate;
+    }
+
+    public void setCriationDate(LocalDate criationDate) {
+        this.criationDate = criationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(cpf, person.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf);
+    }
+}
