8d2b5947e05f2388b0cc85e7cfa3c628e4f5faa4
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-14 19:16:17 -0300

    feat(client): add new constructor using dto

--- back/helpdesk/src/main/java/com/nicolas/helpdesk/domain/Client.java
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/domain/Client.java
@@ -1,12 +1,13 @@
 package com.nicolas.helpdesk.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nicolas.helpdesk.domain.dtos.ClientDTO;
 import com.nicolas.helpdesk.domain.enums.Profile;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Client extends Person {
@@ -22,6 +23,17 @@ public class Client extends Person {
 
     public Client(Integer id, String name, String cpf, String email, String password) {
         super(id, name, cpf, email, password);
+        addProfile(Profile.CLIENT);
+    }
+
+    public Client(ClientDTO clientDTO) {
+        this.id = clientDTO.getId();
+        this.name = clientDTO.getName();
+        this.cpf = clientDTO.getCpf();
+        this.email = clientDTO.getEmail();
+        this.password = clientDTO.getPassword();
+        this.profiles = clientDTO.getProfiles().stream().map(x -> x.getCode()).collect(Collectors.toSet());
+        this.criationDate = clientDTO.getCriationDate();
         addProfile(Profile.CLIENT);
     }
 
