de0ec69a51b4ade806fc93191cf5f43da7a4985f
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-14 19:15:43 -0300

    feat(client): create clientDTO

---
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/domain/dtos/ClientDTO.java
@@ -0,0 +1,102 @@
+package com.nicolas.helpdesk.domain.dtos;
+
+import com.nicolas.helpdesk.domain.Client;
+import com.nicolas.helpdesk.domain.enums.Profile;
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClientDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+
+    @NotBlank(message = "The field Name is required")
+    protected String name;
+
+    @NotBlank(message = "The field Cpf is required")
+    protected String cpf;
+
+    @NotBlank(message = "The field Email is required")
+    protected String email;
+
+    @NotBlank(message = "The field Password is required")
+    protected String password;
+    private Set<Integer> profiles = new HashSet<>();
+    protected LocalDate criationDate = LocalDate.now();
+
+    public ClientDTO() {
+        addProfile(Profile.CLIENT);
+    }
+
+    public ClientDTO(Client client) {
+        this.id = client.getId();
+        this.name = client.getName();
+        this.cpf = client.getCpf();
+        this.email = client.getEmail();
+        this.password = client.getPassword();
+        this.profiles = client.getProfiles().stream().map(x -> x.getCode()).collect(Collectors.toSet());
+        this.criationDate = client.getCriationDate();
+        addProfile(Profile.CLIENT);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Profile> getProfiles() {
+        return profiles.stream().map(x -> Profile.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addProfile(Profile profile) {
+        profiles.add(profile.getCode());
+    }
+
+    public LocalDate getCriationDate() {
+        return criationDate;
+    }
+
+    public void setCriationDate(LocalDate criationDate) {
+        this.criationDate = criationDate;
+    }
+}
