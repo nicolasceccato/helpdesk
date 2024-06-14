d8058a35b3fbb722e0b6ab0de45c79f976685862
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-14 20:08:07 -0300

    feat(ticket): add create

--- back/helpdesk/src/main/java/com/nicolas/helpdesk/resources/TicketResource.java
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/resources/TicketResource.java
@@ -3,12 +3,12 @@ package com.nicolas.helpdesk.resources;
 import com.nicolas.helpdesk.domain.Ticket;
 import com.nicolas.helpdesk.domain.dtos.TicketDTO;
 import com.nicolas.helpdesk.services.TicketService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,4 +34,12 @@ public class TicketResource {
         List<TicketDTO> listOfTicketsDTO = listOfTickets.stream().map(obj -> new TicketDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listOfTicketsDTO);
     }
+
+    @PostMapping
+    public ResponseEntity<TicketDTO> create(@Valid @RequestBody TicketDTO ticketDTO) {
+        Ticket ticket = ticketService.create(ticketDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(ticket.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- back/helpdesk/src/main/java/com/nicolas/helpdesk/services/TicketService.java
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/services/TicketService.java
@@ -1,6 +1,11 @@
 package com.nicolas.helpdesk.services;
 
+import com.nicolas.helpdesk.domain.Client;
+import com.nicolas.helpdesk.domain.Technician;
 import com.nicolas.helpdesk.domain.Ticket;
+import com.nicolas.helpdesk.domain.dtos.TicketDTO;
+import com.nicolas.helpdesk.domain.enums.Priority;
+import com.nicolas.helpdesk.domain.enums.Status;
 import com.nicolas.helpdesk.repositories.TicketRepository;
 import com.nicolas.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,9 +17,13 @@ import java.util.Optional;
 public class TicketService {
 
     private final TicketRepository ticketRepository;
+    private final TechnicianService technicianService;
+    private final ClientService clientService;
 
-    public TicketService(TicketRepository ticketRepository) {
+    public TicketService(TicketRepository ticketRepository, TechnicianService technicianService, ClientService clientService) {
         this.ticketRepository = ticketRepository;
+        this.technicianService = technicianService;
+        this.clientService = clientService;
     }
 
     public Ticket findById(Integer id) {
@@ -25,4 +34,26 @@ public class TicketService {
     public List<Ticket> findAll() {
         return ticketRepository.findAll();
     }
+
+    public Ticket create(TicketDTO ticketDTO) {
+        return ticketRepository.save(newTicket(ticketDTO));
+    }
+
+    private Ticket newTicket(TicketDTO obj) {
+        Technician technician = technicianService.findById(obj.getTechnician());
+        Client client = clientService.findById(obj.getClient());
+
+        Ticket ticket = new Ticket();
+        if (obj.getId() != null) {
+            ticket.setId(obj.getId());
+        }
+        ticket.setTechnician(technician);
+        ticket.setClient(client);
+        ticket.setPriority(Priority.toEnum(obj.getPriority()));
+        ticket.setStatus(Status.toEnum(obj.getStatus()));
+        ticket.setTitle(obj.getTitle());
+        ticket.setObservations(obj.getObservations());
+        return ticket;
+    }
+
 }
