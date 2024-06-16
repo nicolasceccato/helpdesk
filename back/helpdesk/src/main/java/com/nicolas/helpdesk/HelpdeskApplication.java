2424050bf439ae69bca1c054efcb3be3147fee3a
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-16 19:04:43 -0300

    feat(auth): add a bean for BCryptPasswordEncoder

--- back/helpdesk/src/main/java/com/nicolas/helpdesk/HelpdeskApplication.java
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/HelpdeskApplication.java
@@ -2,10 +2,17 @@ package com.nicolas.helpdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HelpdeskApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
