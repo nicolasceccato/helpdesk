2a3874aa42219bfd2e19c2f093b732edcabf93cb
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-16 19:03:58 -0300

    fix(auth): dependency injection made in the constructor and not by @Autowired

--- back/helpdesk/src/main/java/com/nicolas/helpdesk/services/DBService.java
+++ back/helpdesk/src/main/java/com/nicolas/helpdesk/services/DBService.java
@@ -9,7 +9,7 @@ import com.nicolas.helpdesk.domain.enums.Status;
 import com.nicolas.helpdesk.repositories.ClientRepository;
 import com.nicolas.helpdesk.repositories.TechnicianRepository;
 import com.nicolas.helpdesk.repositories.TicketRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -17,20 +17,26 @@ import java.util.Arrays;
 @Service
 public class DBService {
 
-    @Autowired
-    private TechnicianRepository technicianRepository;
+    private final TechnicianRepository technicianRepository;
 
-    @Autowired
-    private ClientRepository clientRepository;
+    private final ClientRepository clientRepository;
 
-    @Autowired
-    private TicketRepository ticketRepository;
+    private final TicketRepository ticketRepository;
+
+    private final BCryptPasswordEncoder encoder;
+
+    public DBService(TechnicianRepository technicianRepository, ClientRepository clientRepository, TicketRepository ticketRepository, BCryptPasswordEncoder encoder) {
+        this.technicianRepository = technicianRepository;
+        this.clientRepository = clientRepository;
+        this.ticketRepository = ticketRepository;
+        this.encoder = encoder;
+    }
 
     public void initDB() {
-        Technician t1 = new Technician(null, "Valdir Cesar", "63653230268", "valdir@mail.com", "123");
+        Technician t1 = new Technician(null, "Valdir Cesar", "63653230268", "valdir@mail.com", encoder.encode("123456789098765432123456789098765432"));
         t1.addProfile(Profile.ADMIN);
 
-        Client c1 = new Client(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", "123");
+        Client c1 = new Client(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", encoder.encode("123"));
 
         Ticket ticket = new Ticket(null, Priority.MEDIUM, Status.IN_PROGRESS, "Chamado 01", "Primeiro chamado", t1, c1);
 
