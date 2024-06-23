dbbb52ac4a2547eedbac7a6c8d924d1845dcd688
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-23 15:26:15 -0300

    feat(login): create the route for login

--- front/helpdesk/src/app/app-routing.module.ts
+++ front/helpdesk/src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
