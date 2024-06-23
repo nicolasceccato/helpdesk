247cc23ad5cbf10ea6be7e8922ba73e9e0526cb1
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-06-23 14:54:30 -0300

    feat(front): add route to technician

--- front/helpdesk/src/app/app-routing.module.ts
+++ front/helpdesk/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'technician', component: TechnicianListComponent }
     ]
   }
 
